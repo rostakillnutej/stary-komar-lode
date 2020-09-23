75072ccfa5901a37afc4ea51cd0ed7f71c56298b
Rostislav <rostakillnutej@gmail.com>
2020-09-23 23:04:36 +0200

    added basic functionality

--- main.py
+++ main.py
@@ -1,10 +1,40 @@
-from flask import Flask
-from flask_socketio import SocketIO
+from flask import Flask, request, send_from_directory
+from flask_socketio import SocketIO, emit
 
-app = Flask(__name__,
-            static_url_path='',
-            static_folder='public')
+app = Flask(__name__, static_url_path='', static_folder='public')
+app.config['SECRET_KEY'] = 'tajny kodes na šifry tady'
+socketio = SocketIO(app)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return app.send_static_file('r_index.html')
+
+'''
+@socketio.on('my event', namespace='/test')
+def test_message(message):
+    emit('my response', {'data': message['data']})
+
+@socketio.on('my broadcast event', namespace='/test')
+def test_message(message):
+    emit('my response', {'data': message['data']}, broadcast=True)
+
+@socketio.on('connect', namespace='/test')
+def test_connect():
+    emit('my response', {'data': 'Connected'})
+
+@socketio.on('disconnect', namespace='/test')
+def test_disconnect():
+    print('Client disconnected')
+'''
+
+
+@socketio.on('change')
+def on_change(data):
+    print('-------------');
+    print(data);
+    print('-------------');
+    emit('change_response', data, broadcast=True)
+
+
+if __name__ == '__main__':
+    socketio.run(app)
